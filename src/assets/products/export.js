2349ed9b928202bb64a1c56870a4c5c9a66dfaf8
CalebBravo <calebphone72@gmail.com>
2024-07-09 23:31:09 -0300

    "Updated image file extensions and removed old image files"

--- src/assets/products/export.js
+++ src/assets/products/export.js
@@ -1,14 +1,14 @@
 import cemento from './cemento.jpg';
 export const cementoBg = cemento;
 
-import hierros from './hierros.jpg';
+import hierros from './hierros.jpeg';
 export const hierrosBg = hierros;
 
 import ladrillos from './ladrillos.jpg';
 export const ladrillosBg = ladrillos;
 
-import loza from './loza-1.jpg';
+import loza from './loza.jpeg';
 export const lozaBg = loza;
 
-import weber from './weber.jpg';
+import weber from './weber.jpeg';
 export const weberBg = weber;
